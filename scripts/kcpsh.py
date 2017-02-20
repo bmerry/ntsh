e621f4e3e77d4562b46ffb4fe263ce6e59fd5371
Bruce Merry <bmerry@gmail.com>
2017-02-20 22:12:45 +0200

    Rewrite using StreamReader/StreamWriter

--- scripts/kcpsh.py
+++ scripts/kcpsh.py
@@ -3,85 +3,80 @@
 import sys
 import contextlib
 import asyncio
-import prompt_toolkit
-from prompt_toolkit.application import Application
-from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.interface import CommandLineInterface
-from prompt_toolkit.layout.containers import VSplit, Window
-from prompt_toolkit.layout.controls import BufferControl, TokenListControl
-from prompt_toolkit.shortcuts import create_asyncio_eventloop, create_prompt_application
+from prompt_toolkit.shortcuts import (
+    create_asyncio_eventloop, create_prompt_application)
 from prompt_toolkit.token import Token
 
 
-class KatcpProtocol(asyncio.Protocol):
-    def __init__(self, cli):
-        self.ready = asyncio.Event()
-        self.connected = False
+class State(object):
+    def __init__(self, cli, reader, writer):
         self._cli = cli
+        self.reader = reader
+        self.writer = writer
 
-    def connection_made(self, transport):
-        self.connected = True
-        self.ready.set()
-        print('Connected')
+    def _print_tokens(self, tokens):
+        self._cli.run_in_terminal(lambda: self._cli.print_tokens(tokens))
 
-    def connection_lost(self, exc):
-        self.ready.set()
-        self.connected = False
-        print('Connection lost')
-        if exc:
-            print(exc)
+    async def _run_reader(self):
+        while True:
+            line = await self.reader.readline()
+            if line == b'':
+                break
+            if line[-1] == b'\n':
+                line = line[:-1]
+            text = line.decode('utf-8', errors='replace')
+            self._print_tokens([(Token.Generic.Inserted, text)])
+        self.writer.close()
 
-    def data_received(self, data):
-        # TODO: split at newlines
-        text = data.decode('utf-8', errors='replace')
-        #sys.stdout.write(data.decode('utf-8', errors='replace'))
-        def print_text():
-            self._cli.print_tokens([(Token.Keyword, text)])
-        self._cli.run_in_terminal(print_text)
+    async def _run_prompt(self):
+        while True:
+            try:
+                command = (await self._cli.run_async()).text
+            except (EOFError, KeyboardInterrupt):
+                break
+            self.writer.writelines([command.encode('utf-8'), b'\n'])
+            await self.writer.drain()
 
-    def pause_writing(self):
-        self.ready.clear()
+    async def run(self):
+        done, pending = await asyncio.wait(
+            [self._run_reader(), self._run_prompt()],
+            return_when=asyncio.FIRST_COMPLETED)
+        for future in done:
+            await future
+        for future in pending:
+            future.cancel()
+            try:
+                await future
+            except asyncio.CancelledError:
+                pass
 
-    def resume_writing(self):
-        self.ready.set()
 
-
-async def test(queue):
-    while True:
-        command = await queue.get()
-
-
-async def main():
-    loop = asyncio.get_event_loop()
+async def async_main():
     application = create_prompt_application(
         '(kcpsh) ',
         enable_history_search=True,)
     with contextlib.closing(create_asyncio_eventloop()) as eventloop:
-        cli = CommandLineInterface(application=application, eventloop=eventloop)
+        cli = CommandLineInterface(application=application,
+                                   eventloop=eventloop)
         sys.stdout = cli.stdout_proxy()
         try:
-            transport, protocol = await loop.create_connection(
-                lambda: KatcpProtocol(cli), 'localhost', 8888)
+            reader, writer = await asyncio.open_connection('localhost', 8888)
         except OSError as error:
-            print('Could not connect to {}: {}'.format('localhost:8888', error.strerror), file=sys.stderr)
+            print('Could not connect to {}: {}'.format(
+                'localhost:8888', error.strerror), file=sys.stderr)
             return 1
-        with contextlib.closing(transport):
-            while True:
-                try:
-                    command = (await cli.run_async()).text
-                    await protocol.ready.wait()
-                    if not protocol.connected:
-                        break
-                    print('Command: {}'.format(command))
-                    transport.writelines([command.encode('utf-8'), b'\n'])
-                except (EOFError, KeyboardInterrupt):
-                    break
+        state = State(cli, reader, writer)
+        with contextlib.closing(writer):
+            await state.run()
     return 0
 
 
-if __name__ == '__main__':
+def main():
     loop = asyncio.get_event_loop()
-    ret = loop.run_until_complete(main())
-    loop.close()
-    sys.exit(ret)
+    with contextlib.closing(loop):
+        return loop.run_until_complete(async_main())
+
+
+if __name__ == '__main__':
+    sys.exit(main())
