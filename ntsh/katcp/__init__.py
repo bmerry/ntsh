51241825f70cd25eae18800cca260bf17d265fa1
Bruce Merry <bmerry@gmail.com>
2018-11-11 12:08:36 +0200

    Update copyright years

--- ntsh/katcp/__init__.py
+++ ntsh/katcp/__init__.py
@@ -1,5 +1,5 @@
 # ntsh: an interactive tool for line-based protocols
-# Copyright (C) 2017  Bruce Merry
+# Copyright (C) 2017, 2018  Bruce Merry
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
